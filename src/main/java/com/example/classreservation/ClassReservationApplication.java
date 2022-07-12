3673ed43a0a55d7768a5b0759896a6c6036daa0c
石川 雅述 <190187@jc-21.jp>
2022-07-12 11:52:00 +0900

    first

---
+++ src/main/java/com/example/classreservation/ClassReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.classreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/classreservation/ClassReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
