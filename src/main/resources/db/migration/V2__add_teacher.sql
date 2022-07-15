08f2e488cbc2d1661b10e0d28ce97231c7f215f8
白鳥 練 <190457@jc-21.jp>
2022-07-15 14:22:46 +0900

    fix:マイグレーションの記述の修正

--- src/main/resources/db/migration/V2__add_teacher.sql
+++ src/main/resources/db/migration/V2__add_teacher.sql
@@ -2,20 +2,20 @@ create table subjects (
     id int not null auto_increment,
     name char(2) not null,
     primary key (id)
-)
+);
 
 create table classrooms (
     id int not null auto_increment,
     name char(3) not null,
-    subjectCode int not null,
+    capacity int not null,
     availableFlag boolean not null,
     primary key (id)
-)
+);
 
-create table teacher (
+create table teachers (
     id int not null auto_increment,
     name varchar(100) not null,
-    subjectCode varchar(100) not null,
+    subjectCode int,
     primary key (id),
-    foreign key subjectCode references subjects(id)
-)
+    foreign key (subjectCode) references subjects(id)
+);
