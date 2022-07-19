81972b3a128facc1198b8690d62449ce4d7996ae
石川 雅述 <190187@jc-21.jp>
2022-07-19 12:32:40 +0900

    Add: 教科データをDBに追加

---
+++ src/main/resources/db/migration/V3__add_subject_data.sql
@@ -0,0 +1,6 @@
+insert into subjects values (1, '国語');
+insert into subjects values (2, '算数');
+insert into subjects values (3, '理科');
+insert into subjects values (4, '社会');
+insert into subjects values (5, '英語');
+insert into subjects values (6, '情報');
