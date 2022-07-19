4c85c10308392ae790fd28d8938e9322adddbf6b
石川 雅述 <190187@jc-21.jp>
2022-07-19 13:50:38 +0900

    SubjectBeanを追加して、TeacherBeanからアクセスできるようにした

--- src/main/java/com/example/classreservation/repository/ClassroomRepository.java
+++ src/main/java/com/example/classreservation/repository/ClassroomRepository.java
@@ -1,7 +1,6 @@
 package com.example.classreservation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.classreservation.bean.ClassroomBean;
 
 public interface ClassroomRepository extends JpaRepository<ClassroomBean, Integer> {
